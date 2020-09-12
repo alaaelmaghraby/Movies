c593157ed00438981a3fa95b9871a017611b929e
NGH11 <hayllie.jul11th.vh@gmail.com>
2020-09-12 11:41:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/moviesappinkotlin/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.moviesappinkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import retrofit2.Callback
+
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    var currentPageNumber = 1
+    lateinit var moviesAdapter: MoviesAdapter
+    lateinit var llm: LinearLayoutManager
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        moviesAdapter = MoviesAdapter(mutableListOf())
+        llm = LinearLayoutManager(this,
+            LinearLayoutManager.VERTICAL,
+            false)
+        rv_movies.adapter = moviesAdapter
+        rv_movies.layoutManager = llm
+        getPopularMovies()
+
+        //rv_movies.adapter = MoviesAdapter()
+//        rv_movies.layoutManager = LinearLayoutManager(this,
+//            LinearLayoutManager.VERTICAL,
+//            false)
+    }
+
+    fun getPopularMovies(){
+        Log.d("Popular Movies",
+            "here" )
+        MoviesClient.fetchPopularMovies(
+            currentPageNumber,
+        ::onPopularMoviesFetched,
+        ::onError)
+    }
+
+    private fun onError() {
+        Toast
+            .makeText(this, "Failed to fetch movies", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun onPopularMoviesFetched(list: MutableList<Movie>) {
+        moviesAdapter.appendMovies(list)
+        attachOnScrollListener()
+    }
+
+    fun attachOnScrollListener(){
+        rv_movies.addOnScrollListener(object: RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val totalItems = llm.itemCount
+                val visibleItemsCount = llm.childCount
+                val firstVisibleItem = llm.findLastVisibleItemPosition()
+
+                if(firstVisibleItem + visibleItemsCount >= totalItems/2){
+                    rv_movies.removeOnScrollListener(this)
+                    currentPageNumber++
+                    getPopularMovies()
+                }
+            }
+        })
+    }
+
+}
